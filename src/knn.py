baf5eb5a5918e2a1ae923c32fe716bddb8f94887
Nnarsipillisakuntala <sakunthalanarsipilli@gmail.com>
2026-04-29 11:18:05 +0530

    Update knn.py

--- src/knn.py
+++ src/knn.py
@@ -1,1 +1,38 @@
 # K-Nearest Neighbors model
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+# Load data
+data = pd.read_csv("data/churn.csv")
+
+if 'customerID' in data.columns:
+    data = data.drop('customerID', axis=1)
+
+data['TotalCharges'] = pd.to_numeric(data['TotalCharges'], errors='coerce')
+data = data.fillna(0)
+
+encoder = LabelEncoder()
+for col in data.columns:
+    if data[col].dtype == "object":
+        data[col] = encoder.fit_transform(data[col])
+
+X = data.drop("Churn", axis=1)
+y = data["Churn"]
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,y,test_size=0.2,random_state=42
+)
+
+# KNN model
+model = KNeighborsClassifier(n_neighbors=5)
+model.fit(X_train,y_train)
+
+y_pred = model.predict(X_test)
+
+print("KNN Accuracy:", accuracy_score(y_test,y_pred))
